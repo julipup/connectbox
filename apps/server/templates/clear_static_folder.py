36cc835b3021e406e257ce26a269bc8f7c3ddc68
julipup <dingolediego@gmail.com>
2023-04-02 12:00:05 +0300

    Fix templates:build pipeline

--- apps/server/templates/clear_static_folder.py
+++ apps/server/templates/clear_static_folder.py
@@ -6,4 +6,7 @@ do_not_delete = [".gitignore"]
 for item in os.listdir(folder):
     item_path = os.path.join(folder, item)
     if item not in do_not_delete:
-        shutil.rmtree(item_path)
+        if os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+        else:
+            os.remove(item_path)
